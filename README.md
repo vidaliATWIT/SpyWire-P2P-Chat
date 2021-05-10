c9f87a9a71a925a828a84ef6a7fd67f1eabfe01a
Vidal <vidali@wit.edu>
2021-05-10 14:26:01 -0400

    Total code refactor. GUI added.

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 ===SPYWIRE P2P Chat Application===
 Designed and programmed by Ian Vidal #vidali@wit.edu
 Additional methods developed by Richard Waltters #waltersr@wit.edu
-Current version: 0.1
+Current version: 0.2
 
 ==Description==
 
-Spywire is a small p2p chat application that allows end-users to communicate via command line. It allows any two users to chat so long as they are both online and know each other's public ip address as well as the configured static IP, in the default case 9323. 
+Spywire is a small p2p chat application that allows end-users to communicate directly. It allows any two users to chat so long as they are both online and know each other's public ip address as well as the configured static IP, in the default case 9091. 
 
 ==Running the program==
 
-Currently the application can only be run by compiling the .py file and running it from the command line. Once done the program itself will ask for a display username and allow you to begin executing commands until the program is closed.
+As of 5/10/2021 the application can now be compiled and used with its accompaniying GUI. Once done the program itself will ask for a display username and allow you to add a peer to your list and start chatting.
 
-==Commands==
+==Notes==
+
+As of 5/10/2021 the command line version of the program is deprecated with develepment now focusing solely on the GUI counterpart. The original command line version is still available in this repo for archival purposes but will be removed once the program goes live with version 1.0 in the coming months. 
+
+Requires socket and tkinter libraries to be installed in order to compile.
+
+==New Additions version 0.2==
+-total refactoring of the code to accomodate a custom gui.
+-encryption is handled slightly differently to provide a more secure experience.
+-removed the ability to share your entire peerlist with another peer. may be added back in the future.
+-when adding a peer you may now specify a name by which to message them instead of using their ip:port.
+
+==New Additions version 0.1==
+-basic rc4 encryption module added..
+-several utility methods related to encryption, decryption and sending added to main file.
+
+
+
+==Commands [DEPRECATED]==
 
 message = chat with a peer 
 check = test connections with your peer list
@@ -20,9 +38,3 @@ send = share your peer list with another peer
 add = add a peer to your peer list
 exit = close the program
 --EXIT = exits the message line
-
-Requires socket library to be installed in order to compile.
-
-==New Additions version 0.1==
--basic rc4 encryption module added..
--several utility methods related to encryption, decryption and sending added to main file.
---
+++ SpyWireCL.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+"""
+@authors: vidali, waltersr
+"""
+
+import socket
+import time
+import random
+import pyrc4
+import pyglet
+from _thread import*
+from tkinter import *
+from tkinter import font
+from tkinter import messagebox
+
+class Peer:
+    inMessage="null"
+    ThreadCount = 0
+    onFlag = True
+    client1Count = 0
+    socketList = []
+    printList = []
+    peerList = [] ##all possible peers in format (ip:port)
+    socketBuffer = []
+    userName = ''
+    key = ""
+    rc4 = None
+    port = 9091 ##Change this number when testing locally via two separate consoles.
+    ##initializes peer class
+    def __init__(self):
+      file = open("PeerList"+str(self.port)+".txt", "a")
+      file.close()
+      self.generateKey()
+      self.initRC4()
+      self.getPeersFromFile()
+    
+    ##main command line loop
+    def mainLoop(self):
+        socket=0
+        start_new_thread(self.serverThread, (self,))
+        time.sleep(.666)
+        self.userName = input('Please input your username: ') ##type your name that will appear when chatting
+        command = ''
+        
+        print("Input 'help' to see list of commands.")
+        
+        while (command!="exit"):
+                try:
+                    command = input("Input a command: " ) ##type a command
+                    if (command=="message"): ##MESSAGE A PEER
+                        peer = input("Which peer would you like to try?") 
+                        if str.isnumeric(peer.split(":")[1]):
+                            self.messagePeer((peer.split(":")[0], int(peer.split(":")[1])))
+                    elif (command=="check"): ##CHECK PEER LIST CONNECTIONS
+                        self.connectCheck()
+                    elif (command=="see"): ##PRINT PEER LIST CONNECTIONS WHO ARE ONLINE
+                        self.printPeerList()
+                    elif (command=="send"): ##SHARE YOUR PEER LIST WITH ANOTHER PEER
+                        peer = input("Which peer would you like to try?")
+                        if str.isnumeric(peer.split(":")[1]):
+                            self.sharePeers((peer.split(":")[0], int(peer.split(":")[1])))
+                    elif (command=="add"): ##ADD ANOTHER PEER TO YOUR LIST
+                        peer = input("Which peer would you like to add?")
+                        self.addPeerToFile((peer.split(":")[0], int(peer.split(":")[1])))
+                    elif (command=="help"): ##PRINT HELP COMMANDS
+                        print("\nmessage = chat with a peer\ncheck = test connections with your peer list\nsee = print peers who are online\nsend = share your peer list with another peer\nadd = add a peer to your peer list\nexit = close the program")
+                    elif (command=="exit"): ##EXIT THE PROGRAM
+                        self.exitServer()
+                except OSError as error: 
+                    print("Peer was not online.")
+                except IndexError as error:
+                    print("Socket not properly initialized.")
+
+                
+    ##client method: message a peer
+    def messagePeer(self, peer):
+        serverName = peer[0]
+        serverPort = peer[1]
+        
+        clientMessage = ""
+        flag = True
+        while (flag):
+            clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            clientSocket.connect((serverName,serverPort))
+            clientMessage = input("Input message: ")
+            if(clientMessage=="--EXIT"):
+                flag=False;
+            else:
+                clientMessage = self.userName + ": " + clientMessage
+                clientMessage = self.mod(clientMessage) ##encrypts message
+            clientSocket.send(clientMessage.encode())
+
+            serverMessage = clientSocket.recv(1024).decode()
+            print(serverMessage)
+            clientSocket.close()
+            
+    ##client method: share peer socket list with another peer
+    def sharePeers(self, peer):
+        serverName = peer[0]
+        serverPort = peer[1]
+        sendStr = ""
+        
+        clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))
+        
+        clientSocket.send("--PEER".encode())
+        time.sleep(3.0)
+
+        for sock in self.socketList:
+            ip = sock[0]
+            port = sock[1]
+            sendStr+= str(ip) + ":" + str(port) + "|"
+        clientSocket.send(sendStr.encode())
+        clientSocket.close()
+     
+    ##server method: receive, allocate, and add peer sockets to socket list
+    def rcvPeerList(self, peerList):
+        print("Reached here")
+        peerArray = peerList.split("|")
+        i=0
+        for peer in peerArray:
+            if (i<len(peerArray)-1):
+                sock = peer.split(":")
+                ##print(sock[0] + ":" + sock[1])
+                self.addPeer(str(sock[0]), int(sock[1]))
+                i+=1
+                time.sleep(3)
+     
+    ##server thread: listening for connections
+    def serverThread(self, nothing):
+        serverMessage = ""
+        serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        serverPort = self.port
+        serverSocket.bind(('', serverPort))
+        
+        serverSocket.listen(5) 
+        print('The server is ready to receive')
+         
+        flag = True
+        
+        while (flag):
+            try:
+                connectionSocket, addr = serverSocket.accept()
+            except OSError as error:
+                print("Could not connect!")
+                break
+            ##if Succeeded
+            ##print ('Connected with ' + addr[0] + ';' + str(addr[1])) ##ip and port
+            clientMessage = connectionSocket.recv(2048).decode() ##1024 is the bytes youll accept for a message
+            if (clientMessage=="--PEER"):
+                peerList = connectionSocket.recv(2048).decode()
+                time.sleep(0.666)
+                connectionSocket.close()
+                self.rcvPeerList(peerList)
+                ##print("DONE")
+
+            elif (clientMessage=="--EXIT"):
+                connectionSocket.close()
+            elif (clientMessage=='--QUIT'):
+                flag=False
+            else:
+                clientMessage = self.rcvmod(clientMessage) ##decrypts message
+                print(clientMessage)
+                connectionSocket.send((clientMessage).encode())
+        connectionSocket.close()
+        serverSocket.close()
+    
+    ##server method: add a peer's socket to socket list
+    def addPeer(self, serverName, portNumber):
+        server = serverName
+        port = int(portNumber)
+        addFlag = True
+        
+        for sock in self.socketList:
+            compStr1 = str(sock[0]) + ":" + str(sock[1])
+            compStr2 = str(server) + ":" + str(port)
+            if (compStr1 == compStr2 or port==self.port):
+                addFlag=False
+        if(addFlag):
+            self.socketList.append((server, port))
+            ##self.addPeerToFile((server, port)) ##saving shared peers to file
+        else:
+            print("Duplicate socket found! Not Copied.")
+            
+    ##client method: verify connections to sockets in socket list
+    def connectCheck(self):
+        self.printList.clear()
+        for socketPair in self.socketList:
+            testSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            
+            try:
+                testSocket.connect(socketPair)
+                testSocket.send("--EXIT".encode())
+            except socket.error as error: 
+                print("Could not connect to " + str(socketPair[0]) +":" + str(socketPair[1]))
+            else: 
+                self.printList.append(str(socketPair[0]) +":" + str(socketPair[1]))
+    
+            testSocket.close()
+            
+    ##client method: print which peers are online        
+    def printPeerList(self):
+        for peer in self.printList:
+            print(peer + " is online!")
+            
+    ##client method: exit the program
+    def exitServer(self):
+       clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+       clientSocket.connect(('localhost',self.port))
+       clientSocket.send("--QUIT".encode())
+        
+    ##client method: add peer socket to file
+    def addPeerToFile(self, newPeer):
+        duplicate = False
+        self.getPeersFromFile()
+        for socketPair in self.socketList:
+            if socketPair[0] == newPeer[0] and socketPair[1] == newPeer[1]:
+                duplicate = True
+                print ("Duplicate socket detected. Not copied.")
+        if not duplicate:
+            socketListFile = open("PeerList"+str(self.port)+".txt", "a")
+            socketListFile.write(str(newPeer[0]) + ":" + str(newPeer[1]) + "\n")
+            self.socketList.append((str(newPeer[0]), int(newPeer[1])))
+            socketListFile.close()
+            
+    ##client method: retreive peer list sockets from file
+    def getPeersFromFile (self):
+        duplicate = False
+        socketListFile = open("PeerList"+str(self.port)+".txt", "r")
+        socketPairs = socketListFile.readlines()
+        for socketPair in socketPairs:
+            duplicate = False
+            for peer in self.socketList:
+                if socketPair.split(":")[0] == peer[0] and int(socketPair.split(":")[1]) == peer[1]:
+                    duplicate = True
+            if not duplicate:
+                self.socketList.append((socketPair.rsplit(":")[0],int(socketPair.split(":")[1])))
+            self.peerList.append(socketPair)
+        socketListFile.close()
+        
+    ##Encryption methods
+    
+    def generateKey(self): ##generates a new random key every session
+        key = 0
+        for i in range(6):
+            key+= (pow(10,i))*random.randint(1, 9)
+        self.key = str(key)
+        
+    def initRC4(self): ##initialize the rc4 object and generate the keyStream for this session
+        self.rc4 = pyrc4.rc4(self.key)
+        
+    def ixor(self, message): ##xor with internal keyStream
+        return self.rc4.mxor(message)
+    
+    def exor(self, message, keyStream): ##xor with external keyStream
+        return self.rc4.uXOR(message, keyStream)
+    
+    def mod(self, message): ##encrypts message and modifies for sending
+        message = self.rc4.getString(self.ixor(message))
+        print("Start of message: " + message + "END OF MESSAGE")
+        return message + "??x" + self.rc4.keyStream
+    
+    def rcvmod(self, message): ##receives modified message, splits and de-encrypts
+        msgList = message.split("??x")
+        return self.rc4.getString(self.exor(msgList[0], msgList[1]))
+        
+            
+            
+s = Peer() ##creates the Peer object
+s.mainLoop() ##runs the main loop
